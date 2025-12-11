fa1d084b2dd666a9aeda4f4ff11add633ce8b126
Kebron Gizachew <kebisel21@gmail.com>
2025-12-11 13:26:38 +0300

    adding a post, get methods

--- db/routes/user.js
+++ db/routes/user.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import {getNotes} from '../controller/nextCont.js'
+import {getNotes,createNotes} from '../controller/nextCont.js'
 const router=express.Router()
-router.get('/notes',getNotes)
+router.route('/notes').get(getNotes).post(createNotes)
 export default router
